2aeb5503d179dafb25b1b0cac2dd1479000a9724
albinlindskog <albin.lindskog@live.se>
2021-12-12 15:18:01 +0100

    Ensure project builds

--- source/javascripts/app/_dynamic.js
+++ source/javascripts/app/_dynamic.js
@@ -1,8 +1,11 @@
-function wrapDynamicContent() {
+//= require ../lib/_jquery
+
+;(function wrapDynamicContent() {
+  'use strict';
   dynamicServerNames();
   dynamicAuthenticationToken();
   dynamicIntegrationId();
-}
+})();
 
 /*
 When the page has loaded we wrap all occurrences of the server name with a <span/> then we attach
@@ -10,22 +13,22 @@ an event_handler to the serverSelect in index.html.md. This allows us the user t
 the prod and demo server in the documentation.
  */
 function dynamicServerNames() {
-  let input = $('input[type=radio]');
-  let placeholder = 'https://api.gigapay.se/v2/';
-  let value = $('input[type=radio]:checked')[0].value.replace('///', '//');  // remove escaping necessary in .md file
+  var input = $('input[type=radio]');
+  var placeholder = 'https://api.gigapay.se/v2/';
+  var value = $('input[type=radio]:checked')[0].value.replace('///', '//');  // remove escaping necessary in .md file
 
-  let span = document.createElement('span');
+  var span = document.createElement('span');
   span.className = 'hostServer';
   span.innerHTML = value || placeholder;
 
-  for (const element of document.querySelectorAll("code")) {
-    if (element.innerText.includes(placeholder)) {
-      element.innerHTML = element.innerHTML.replaceAll(placeholder, span.outerHTML);
+  $("code").each(function() {
+    if (this.innerText.includes(placeholder)) {
+      this.innerHTML = this.innerHTML.replaceAll(placeholder, span.outerHTML);
     }
-  }
+  });
 
   input.click(function() {
-    let choice = this.value.replace('///', '//');
+    var choice = this.value.replace('///', '//');
     $(".hostServer").map(function() {
       this.innerText = choice;
     })
@@ -36,22 +39,22 @@ function dynamicServerNames() {
 Then we do the same for the Authentication Token input.
  */
 function dynamicAuthenticationToken() {
-  let inputs = $('#auth')
-  let placeholder =  inputs[0].placeholder
-  let value = inputs[0].value
+  var inputs = $('#auth')
+  var placeholder =  inputs[0].placeholder
+  var value = inputs[0].value
 
-  let span = document.createElement('span');
+  var span = document.createElement('span');
   span.className = 'authenticationToken';
   span.innerHTML = value || placeholder;
 
-  for (const element of document.querySelectorAll("code")) {
-    if (element.innerText.includes(placeholder)) {
-      element.innerHTML = element.innerHTML.replaceAll(placeholder, span.outerHTML);
+  $("code").each(function() {
+    if (this.innerText.includes(placeholder)) {
+      this.innerHTML = this.innerHTML.replaceAll(placeholder, span.outerHTML);
     }
-  }
+  });
 
   inputs.on('input', function() {
-    let value = this.value;
+    var value = this.value;
     $(".authenticationToken").map(function() {
       this.innerText = value ? value : placeholder;
     })
@@ -62,22 +65,22 @@ function dynamicAuthenticationToken() {
 And the Integration ID input.
  */
 function dynamicIntegrationId() {
-  let inputs = $('#integration');
-  let placeholder =  inputs[0].placeholder
-  let value = inputs[0].value
+  var inputs = $('#integration');
+  var placeholder =  inputs[0].placeholder
+  var value = inputs[0].value
 
-  let span = document.createElement('span');
+  var span = document.createElement('span');
   span.className = 'integrationToken';
   span.innerHTML = value || placeholder;
 
-  for (const element of document.querySelectorAll("code")) {
-    if (element.innerText.includes(placeholder)) {
-      element.innerHTML = element.innerHTML.replaceAll(placeholder, span.outerHTML);
+  $("code").each(function() {
+    if (this.innerText.includes(placeholder)) {
+      this.innerHTML = this.innerHTML.replaceAll(placeholder, span.outerHTML);
     }
-  }
+  });
 
   inputs.on('input', function() {
-    let value = this.value;
+    var value = this.value;
     $(".integrationToken").map(function() {
       this.innerText = value ? value : placeholder;
     })
